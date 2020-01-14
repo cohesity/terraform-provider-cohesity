d7fa07a36b9596400fc6db1d536f6c026a53f9b7
Gaurav Gupta Maplelabs <cohesity@view-gaurav.gupta.maplelabs-ubu>
2020-01-14 22:29:32 +0530

    Completed Source_PhysicalServer workflow

---
+++ cohesity/resource_cohesity_source_physicalServer.go
@@ -0,0 +1,136 @@
+package cohesity
+
+import (
+	"errors"
+	"log"
+	"strconv"
+
+	CohesityManagementSdk "github.com/cohesity/management-sdk-go/managementsdk"
+	"github.com/cohesity/management-sdk-go/models"
+	"github.com/hashicorp/terraform/helper/schema"
+)
+
+func resourceCohesitySourcePhysicalServer() *schema.Resource {
+	return &schema.Resource{
+		Create: resourceCohesitySourcePhysicalServerCreate,
+		Read:   resourceCohesitySourcePhysicalServerRead,
+		Delete: resourceCohesitySourcePhysicalServerDelete,
+		Update: resourceCohesitySourcePhysicalServerUpdate,
+		Schema: map[string]*schema.Schema{
+			"endpoint": {
+				Type:     schema.TypeString,
+				Required: true,
+				Description: `Specifies the network endpoint of the Protection
+				Source where it is reachable. It could be an URL or hostname or
+				an IP address of the Protection Source`,
+			},
+			"force_register": {
+				Type:        schema.TypeBool,
+				Optional:    true,
+				Default:     true,
+				Description: `Forcefully register physical server to target cluster`,
+			},
+			"host_type": {
+				Type:        schema.TypeString,
+				Required:    true,
+				Description: "OS type on the physical server.",
+			},
+			"physical_type": {
+				Type:        schema.TypeString,
+				Required:    true,
+				Description: "Host Type for physical server.",
+			},
+		},
+	}
+}
+
+func resourceCohesitySourcePhysicalServerCreate(resourceData *schema.ResourceData, configMetaData interface{}) error {
+	log.Printf("[INFO] Starting PS Registration")
+	var cohesityConfig = configMetaData.(Config)
+	client, err := CohesityManagementSdk.NewCohesitySdkClient(cohesityConfig.clusterVip,
+		cohesityConfig.clusterUsername, cohesityConfig.clusterPassword, cohesityConfig.clusterDomain)
+	if err != nil {
+		log.Printf(err.Error())
+		return errors.New("Failed to authenticate with Cohesity")
+	}
+	var endpoint = resourceData.Get("endpoint").(string)
+	var forceRegister = resourceData.Get("force_register").(bool)
+
+	//Creating Request Body
+	var requestBody models.RegisterProtectionSourceParameters
+	requestBody.Endpoint = &endpoint
+	requestBody.ForceRegister = &forceRegister
+	requestBody.Environment = models.EnvironmentRegisterProtectionSourceParameters_KPHYSICAL
+	requestBody.HostType = models.HostTypeRegisterProtectionSourceParameters_KLINUX
+	requestBody.PhysicalType = models.PhysicalType_KHOST
+
+	log.Printf("[INFO] Register Physical Server as protection source %s", endpoint)
+	result, err := client.ProtectionSources().CreateRegisterProtectionSource(&requestBody)
+
+	if err != nil {
+		log.Printf(err.Error())
+		return errors.New("Failed to register Cohesity protection source")
+	}
+	resourceData.SetId(strconv.FormatInt(*result.Id, 10))
+	log.Printf("[INFO] Successfully registered physical server protection source %s", endpoint)
+	log.Printf("[INFO] ID %s", resourceData.Id())
+	return resourceCohesitySourcePhysicalServerRead(resourceData, configMetaData)
+}
+
+func resourceCohesitySourcePhysicalServerRead(resourceData *schema.ResourceData, configMetaData interface{}) error {
+	log.Printf("Starting Read")
+	var cohesityConfig = configMetaData.(Config)
+	client, err := CohesityManagementSdk.NewCohesitySdkClient(cohesityConfig.clusterVip,
+		cohesityConfig.clusterUsername, cohesityConfig.clusterPassword, cohesityConfig.clusterDomain)
+	if err != nil {
+		log.Printf(err.Error())
+		return errors.New("Failed to authenticate with Cohesity")
+	}
+	var trueValue = true
+	var environmentType = []models.EnvironmentListProtectionSourcesEnum{models.EnvironmentListProtectionSources_KPHYSICAL}
+	var endpoint = resourceData.Get("endpoint").(string)
+	protectionSourceID, _ := strconv.ParseInt(resourceData.Id(), 10, 64)
+	log.Printf("[INFO] Read Cohesity Physical Server protection source %d", protectionSourceID)
+
+	result, err := client.ProtectionSources().ListProtectionSources(&protectionSourceID, nil, nil, &trueValue, &trueValue, &trueValue, environmentType, nil, &trueValue, nil, nil, &trueValue)
+
+	for _, protectionSource := range result {
+		log.Printf("[INFO] Protection Name: %s", *protectionSource.ProtectionSource.Name)
+		if *protectionSource.ProtectionSource.Name == endpoint {
+			log.Printf("[INFO] Found the Physical Server protection source %s on cohesity cluster",
+				*protectionSource.ProtectionSource.Name)
+			return nil
+		}
+	}
+	log.Printf("[INFO] Couldn't find the Physical Server protection source %s", err)
+	resourceData.SetId("")
+	return nil
+}
+
+func resourceCohesitySourcePhysicalServerDelete(resourceData *schema.ResourceData, configMetaData interface{}) error {
+	log.Printf("Starting Delete")
+	var cohesityConfig = configMetaData.(Config)
+	client, err := CohesityManagementSdk.NewCohesitySdkClient(cohesityConfig.clusterVip,
+		cohesityConfig.clusterUsername, cohesityConfig.clusterPassword, cohesityConfig.clusterDomain)
+	if err != nil {
+		log.Printf(err.Error())
+		return errors.New("Failed to authenticate with Cohesity")
+	}
+
+	protectionSourceID, _ := strconv.ParseInt(resourceData.Id(), 10, 64)
+	log.Printf("[INFO] Unregistering the Physical Server protection source %s", resourceData.
+		Get("endpoint").(string))
+
+	err = client.ProtectionSources().DeleteUnregisterProtectionSource(protectionSourceID)
+	if err != nil {
+		log.Printf(err.Error())
+		return errors.New("Failed to unregister Physical Server protection source")
+	}
+	log.Printf("[INFO] Successfully unregistered the Physical Server protection source %s",
+		resourceData.Get("endpoint").(string))
+	return nil
+}
+
+func resourceCohesitySourcePhysicalServerUpdate(resourceData *schema.ResourceData, configMetaData interface{}) error {
+	return nil
+}
